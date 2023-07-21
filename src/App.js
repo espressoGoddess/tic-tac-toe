58dde0db0ca0981eaf0be572e507039e6ed8ebf0
Amber Shipley <ashipley2@me.com>
2023-07-21 14:32:35 -0700

    Create turn property and pass to children

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ import { useState, useEffect } from "react";
 function App() {
   const [player1, setPlayer1] = useState({});
   const [player2, setPlayer2] = useState({});
+  const [turn, setTurn] = useState('player1');
   useEffect(() => {
-    setPlayer1({name: 'Amber', score: 0});
-    setPlayer2({name: 'Nathan', score: 0});
+    setPlayer1({name: 'Amber', score: 0, isTurn: true, token: 'X'});
+    setPlayer2({name: 'Nathan', score: 0, isTurn: false, token: 'O'});
   }, []);
 
   return (
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,10 +1,23 @@
 import { useState } from "react";
 import Box from "./Box";
 
-export default function GameBoard() {
+export default function GameBoard({ turn, setTurn }) {
 
   const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
-  const boxes = board.map((item, index) => <Box value={item} key={index}/>)
+
+  const boxes = board?.map((item, index) => {
+    return (
+      <Box
+        value={item}
+        key={index}
+        index={index}
+        turn={turn}
+        setTurn={setTurn}
+        board={board}
+        setBoard={setBoard}
+      />
+    );
+  })
 
   return (
     <section className='GameBoard'>
