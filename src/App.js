775c0cfbe13a742fd6b41f4cc68da96eaee61a42
Amber Shipley <ashipley2@me.com>
2023-07-25 17:25:13 -0700

    Check for game winner and end game

--- src/App.js
+++ src/App.js
@@ -5,11 +5,26 @@ import { useState } from "react";
 function App() {
   const [player1, setPlayer1] = useState({name: 'Amber', score: 0, token: 'X'});
   const [player2, setPlayer2] = useState({name: 'Nathan', score: 0, token: 'O'});
+  const [winner, setWinner] = useState(null);
+
+
+  const handleGameEnd = winnerToken => {
+    if (winnerToken === player1.token) {
+      setWinner(player1.name);
+      setPlayer1({...player1, score: player1.score + 1})
+    } else if (winnerToken === player2.token) {
+      setWinner(player2.name);
+      setPlayer2({...player2, score: player2.score + 1})
+    } else if(winnerToken === 'Draw') {
+      setWinner('Draw');
+    }
+  }
 
   return (
     <main className="App">
       <ScoreCard player={player1}/>
-      <GameBoard player1={player1} player2={player2} />
+      {winner}
+      <GameBoard onGameEnd={handleGameEnd} player1={player1} player2={player2} disabled={winner} />
       <ScoreCard player={player2}/>
     </main>
   );
--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import Box from "./Box";
 
-export default function GameBoard({ player1, player2 }) {
+export default function GameBoard({ player1, player2, onGameEnd, disabled = false }) {
 
   const [board, setBoard] = useState(['', '', '', '', '', '', '', '', '']);
   const [turn, setTurn] = useState(player1);
@@ -19,7 +19,12 @@ export default function GameBoard({ player1, player2 }) {
     const newBoard = [...board];
     newBoard[index] = turn.token;
     setBoard(newBoard);
-    nextTurn();
+    const winnerToken = checkForWin(newBoard);
+    if (winnerToken) {
+      onGameEnd(winnerToken);
+    } else {
+      nextTurn();
+    }
   }
 
   const boxes = board?.map((item, index) => {
@@ -27,6 +32,7 @@ export default function GameBoard({ player1, player2 }) {
       <Box
         value={item}
         key={index}
+        disabled={disabled}
         onClick={() => handleClick(index)}
       />
     );
@@ -45,4 +51,27 @@ export default function GameBoard({ player1, player2 }) {
       <button onClick={resetBoard}>Reset Board</button>
     </section>
   );
+}
+
+function checkForWin(gameboard) {
+  const winningCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 4, 6],
+    [2, 5, 8],
+  ];
+
+  for (let i = 0; i < winningCombos.length; i ++) {
+    const [x, y, z] = winningCombos[i];
+    if (gameboard[x] && gameboard[x] === gameboard[y] && gameboard[x] === gameboard[z]) {
+      return gameboard[x];
+    }
+  }
+  if (gameboard.every(item => item)) {
+    return 'Draw';
+  }
 }
